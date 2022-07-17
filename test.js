3d8f7e21c66b7c328dba3d42454b365f56f192fe
Dmitry <dzm.krasnov@gmail.com>
2022-07-17 19:04:57 +0300

    Init repo

---
+++ test.js
@@ -0,0 +1,10 @@
+const {chromium} = require('playwright');
+
+(async() => {
+    const browser = await chromium.launch()
+    const context = await browser.newContext();
+    const page = await context.newPage();
+    await page.goto('https://www.onliner.by/')
+    await page.screenshot({ path: 'page.png'});
+    await browser.close();
+}) ()
